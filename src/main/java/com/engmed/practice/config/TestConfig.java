8c2a082c6694b4b71e620ac0cc7f9f240e08a358
Juan Oliveira <jfs.dev.eng@gmail.com>
2021-12-24 10:48:14 -0300

    orderitem pt2

--- src/main/java/com/engmed/practice/config/TestConfig.java
+++ src/main/java/com/engmed/practice/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.engmed.practice.entities.Category;
 import com.engmed.practice.entities.Order;
+import com.engmed.practice.entities.OrderItem;
 import com.engmed.practice.entities.Product;
 import com.engmed.practice.entities.User;
 import com.engmed.practice.entities.enums.OrderStatus;
 import com.engmed.practice.repositories.CategoryRepository;
+import com.engmed.practice.repositories.OrderItemRepository;
 import com.engmed.practice.repositories.OrderRepository;
 import com.engmed.practice.repositories.ProductRepository;
 import com.engmed.practice.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -65,6 +70,13 @@ public class TestConfig implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
--- src/main/java/com/engmed/practice/entities/OrderItem.java
+++ src/main/java/com/engmed/practice/entities/OrderItem.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import com.engmed.practice.entities.pk.OrderItemPk;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_order_item")
@@ -17,7 +18,8 @@ public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@EmbeddedId
-	private OrderItemPk id;
+	private OrderItemPk id = new OrderItemPk();
+	
 	private Integer quantity;
 	private Double price;
 	
@@ -33,6 +35,7 @@ public class OrderItem implements Serializable {
 		
 	}
 	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
